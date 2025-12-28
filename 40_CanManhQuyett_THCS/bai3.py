203655e2d1460a861d56c4d1da717088882d8d20
cmquyet25174600163-code <cmquyet.25174600163@sv.uneti.edu.vn>
2025-12-28 23:39:02 +0700

    Delete 40_CanManhQuyett_THCS directory

--- 40_CanManhQuyett_THCS/bai3.py
+++
@@ -1,5 +0,0 @@
-ds_so = [2, 4, 5, 7, 9, 8]
-with open("so_nguyen.txt", "w", encoding="utf-8") as f:
-    for so in ds_so:
-        f.write(str(so) + "\n")
-print("Đã ghi danh sách số nguyên vào file.")
--- 40_CanManhQuyett_THCS/bai5.py
+++
@@ -1,8 +0,0 @@
-with open("vanban.txt", "rb") as file_nguon, open("ban_sao.txt", "wb") as file_dich:
-    while True:
-        du_lieu = file_nguon.read(1024)
-        if not du_lieu:
-            break
-        file_dich.write(du_lieu)
-
-print("Đã sao chép file thành công.")
