1321c310d1ac224fef29569d18ac265a6d193429
cmquyet25174600163-code <cmquyet.25174600163@sv.uneti.edu.vn>
2025-12-28 20:05:34 +0700

    Delete 40_CanManhQuyet_THCS 19.25.44 directory

--- 40_CanManhQuyet_THCS 19.25.44/bai1/main.py
+++
@@ -1,5 +0,0 @@
-from hinh_hoc import chu_vi_hinh_tron,chu_vi_hinh_vuong
-ChuViHinhVuong = chu_vi_hinh_vuong(5)
-ChuViHinhTron = chu_vi_hinh_tron(3)
-print(f"chu vi hình vuông là: {ChuViHinhVuong}")
-print(f"chu vi hình tròn là: {ChuViHinhTron}")
