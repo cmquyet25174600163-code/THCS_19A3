ebedd83f1bfac782f1da0111664cb15ce479ee2a
cmquyet25174600163-code <cmquyet.25174600163@sv.uneti.edu.vn>
2025-12-28 20:06:05 +0700

    Add files via upload

---
+++ 40_CanManhQuyet_THCS 19.25.44/bai2/main.py
@@ -0,0 +1,3 @@
+from chuoi_utility import dao_nguoc_chuoi
+chu = "Cấn Mạnh Quyết"
+print("kết quả đảo ngược: ",dao_nguoc_chuoi(chu))
---
+++ 40_CanManhQuyet_THCS 19.25.44/bai3/su_dung.py
@@ -0,0 +1,8 @@
+from may_tinh.co_ban import *
+from may_tinh.nang_cao import *
+a,b = 10,9
+c = 9
+print("phép cộng: ",cong(a,b))
+print("phép trừ: ",tru(a,b))
+print("lũy thừa: ",luy_thua(a,b))
+print("căn bậc 2: ",can_bac_hai(c))
